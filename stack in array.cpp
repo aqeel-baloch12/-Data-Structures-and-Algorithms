d05c5e8b034bc160ba805dd25d003f8f102830d3
Aqeel Baloch <aqeelsuleman310@gmail.com>
2024-10-07 03:55:48 -0700

    stack operation in array

---
+++ stack in array.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+int stack[5],n=5,top=-1;
+void push(int x)
+{
+	if(top==n-1)
+	{
+		cout<<"stack is full";
+	}
+	else
+	{
+		top++;
+		stack[top]=x;
+	}
+}
+void display()
+{
+	if(top>=0)
+	{
+		cout<<"stack element are";
+		for(int i=top;i>=0;i--)
+		{
+			cout<<stack[i]<<endl;
+		}
+	}
+	else
+	{
+		cout<<"stack is empty";
+	}
+}
+
+int main()
+{
+	push(1);
+	push(2);
+	push(3);
+	push(4);
+	push(5);
+	display();
+	return 0;
+}
